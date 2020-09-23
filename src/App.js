c1583b174bc73fe7e78a4b8d25516dabf3ed3938
chsprithvi <chsprithvi@gmail.com>
2020-09-23 20:46:06 +0530

    Refactored

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and savaaahgae to reload.
+          Edit <code>src/xxApp.js</code> and savaaahgae to reload.
         </p>
         <a
           className="App-link"
